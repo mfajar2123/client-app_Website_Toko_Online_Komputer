d235bd738a0fa2bb37154431e881e2297b9ba878
Ilham <muhammadilhammahgribi@gmail.com>
2022-12-14 15:40:13 +0700

    initial client app

---
+++ src/main/java/com/clientappkelompok3/ClientappKelompok3Application.java
@@ -0,0 +1,14 @@
+package com.clientappkelompok3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientappKelompok3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientappKelompok3Application.class, args);
+                System.out.println("Client app is running . . . . .");
+	}
+
+}
