06cf887ac4d8cccecb949276551bb07b764d075c
Ilham <muhammadilhammahgribi@gmail.com>
2022-12-14 15:58:39 +0700

    rest template

---
+++ src/main/java/com/clientappkelompok3/config/RestTemplateConfig.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clientappkelompok3.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Ilham
+ */
+
+@Configuration
+public class RestTemplateConfig {
+    
+    @Bean
+    public  RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
