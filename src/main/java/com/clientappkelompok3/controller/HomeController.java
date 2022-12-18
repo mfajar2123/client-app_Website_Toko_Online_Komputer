564f536b8020904d8a4b8a2bc88f6e01dd20259d
Ilham <muhammadilhammahgribi@gmail.com>
2022-12-18 20:43:58 +0700

    tampil produk di home

--- src/main/java/com/clientappkelompok3/controller/HomeController.java
+++ src/main/java/com/clientappkelompok3/controller/HomeController.java
@@ -5,23 +5,30 @@
  */
 package com.clientappkelompok3.controller;
 
+import com.clientappkelompok3.service.KategoriService;
 import com.clientappkelompok3.service.ProdukService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author Ilham
  */
 @Controller
+@AllArgsConstructor
+@RequestMapping("/home")
 public class HomeController {
-    
+
     private ProdukService produkService;
-    
-    @GetMapping("/home")
-    public String home(Model model){
+    private KategoriService kategoriService;
+
+    @GetMapping
+    public String home(Model model) {
         model.addAttribute("produk", produkService.getAll());
+        model.addAttribute("kategori", kategoriService.getAll());
         return "index";
-    }   
+    }
 }
